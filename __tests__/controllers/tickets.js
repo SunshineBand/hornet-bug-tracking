5cc8d97f6d53bdd503b6941bd01ad80851bcb347
PushBtnsDev <kyledchenoweth@gmail.com>
2019-11-29 08:06:21 +1100

    building ticket create logic

--- __tests__/controllers/tickets.js
+++ __tests__/controllers/tickets.js
@@ -1,10 +1,15 @@
-const { index } = require('../../src/controllers/tickets.js');
+const {
+  index,
+  create
+} = require('../../src/controllers/tickets.js');
+
 const Ticket = require('../../src/models/Ticket');
 const ticketMockDatabase = require('../../__mocks__/tickets');
 
 jest.mock('../../src/models/Ticket', () => {
   return {
-    find: jest.fn()
+    find: jest.fn(),
+    save: jest.fn()
   }
 });
 
@@ -49,6 +54,54 @@ describe('ticket controller', () => {
       }
       catch (err) {
         expect(err).toBeDefined();
+      }
+    })
+  })
+
+  describe('create', () => {
+
+    const req = {
+      mock: 'request'
+    }
+    
+    it('requires a request object', async () => {
+      expect.assertions(1);
+      try {
+        const result = await create();
+        expect(result).toBeUndefined();
+      }
+      catch (e) {
+        expect(e).toBeDefined();
+      }
+    })
+
+    it('requires a title and issueType in request', async done => {
+      expect.assertions(1);
+      const req = {
+        mock: 'request'
+      }
+
+      try {
+        const result = await create(req);
+      }
+      catch (e) {
+        expect(e).toBeDefined();
+        done();
+      }
+    })
+
+    it('creates a new ticket and saves to database', async () => {
+      const req = {
+        title: "New Ticket",
+        issueType: 'Feature'
+      }
+
+      try {
+        const result = await create(req);
+        expect(Ticket.save).toHaveBeenCalled();
+      }
+      catch (e) {
+        expect(e).toBeUndefined();
       }
     })
   })
--- src/controllers/tickets.js
+++ src/controllers/tickets.js
@@ -11,6 +11,32 @@ const index = async (req, res, next) => {
   }
 }
 
+const create = async (req, res, next) => {
+  if(!req) {
+    throw new Error('Expected a request object');
+  }
+
+  const { title, issueType, description } = req;
+
+  if(!title || !issueType) {
+    throw new Error('Expected a title and an issueType');
+  }
+
+  const ticket = new Ticket({
+    title: title,
+    issueType: issueType,
+    description: description
+  })
+
+  try {
+    const newTicket = await ticket.save();
+  }
+  catch (e) {
+    throw e
+  }
+}
+
 module.exports = {
-  index
+  index,
+  create
 }
